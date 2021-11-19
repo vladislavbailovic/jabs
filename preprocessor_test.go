f29c9e03125f059334cde12d928e4e1db99e1dba
vladislav <ve@incsub.com>
2021-11-19 20:25:06 +0100

    Test total macros count

--- preprocessor_test.go
+++ preprocessor_test.go
@@ -1,12 +1,27 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test_Preprocessor(t *testing.T) {
 	p := NewPreprocessor("./examples/first.yml")
+	expectedMacros := 7
+	actualMacros := 0
 	for name, macro := range p.Macros {
 		if len(macro.Value) > 100 {
 			t.Fatalf("macro value length too long for: %s (%d)", name, len(macro.Value))
 		}
+		actualMacros += 1
+	}
+	if expectedMacros != actualMacros {
+		fmt.Println("Macros")
+		fmt.Println("--------------------")
+		for name, _ := range p.Macros {
+			fmt.Printf("%s\n", name)
+		}
+		fmt.Println("--------------------")
+		t.Fatalf("expected %d macros, but got %d instead", expectedMacros, actualMacros)
 	}
 }
