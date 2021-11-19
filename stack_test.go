4d936d69ad3cfa34df47b7bde97389b6d59475df
vladislav <ve@incsub.com>
2021-11-19 18:35:07 +0100

    About to introduce observables

---
+++ stack_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_EvaluationStack_Dependent(t *testing.T) {
+	p := NewPreprocessor("./examples/first.yml")
+	es := NewEvaluationStack("root", p.Rules)
+	expectedRules := 4
+	if len(es.stack) != expectedRules {
+		fmt.Println("Stack")
+		fmt.Println("--------------------")
+		for _, rl := range es.stack {
+			fmt.Printf("\t- %s\n", rl.Name)
+		}
+		fmt.Println("--------------------")
+		t.Fatalf("expected %d rules, but got %d", expectedRules, len(es.stack))
+	}
+}
+
+func Test_EvaluationStack_Standalone(t *testing.T) {
+	p := NewPreprocessor("./examples/first.yml")
+	es := NewEvaluationStack("Standalone rule", p.Rules)
+	expectedRules := 1
+	if len(es.stack) != expectedRules {
+		fmt.Println("Stack")
+		fmt.Println("--------------------")
+		for _, rl := range es.stack {
+			fmt.Printf("\t- %s\n", rl.Name)
+		}
+		fmt.Println("--------------------")
+		t.Fatalf("expected %d rules, but got %d", expectedRules, len(es.stack))
+	}
+}
