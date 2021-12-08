78b752b5c2013ae44a8f794e2f36a163cc258dd5
vladislav <ve@incsub.com>
2021-12-08 05:54:18 +0100

    Implement fileish for glob file reading

--- sys/fs.go
+++ sys/fs.go
@@ -2,6 +2,7 @@ package sys
 
 import (
 	"io/ioutil"
+	"path/filepath"
 	"jabs/dbg"
 )
 
@@ -17,4 +18,50 @@ func TempFilePath() string {
 	}
 	file.Close()
 	return file.Name()
+}
+
+type Fileish struct {
+	path string
+	sources []string
+}
+
+func NewFileish(path string) Fileish {
+	matches, err := filepath.Glob(path)
+	if err != nil {
+		dbg.Error("Unable to resolve path spec: %s (%v)", path, err)
+	}
+	if len(matches) == 0 {
+		dbg.Error("Could not resolve path spec to concrete files: %s", path)
+	}
+	sources := []string{}
+	for _, match := range matches {
+		abs, err := filepath.Abs(match)
+		if err != nil {
+			dbg.Warning("Could not resolve absolute path for %s: %v", match, err)
+			continue
+		}
+		ext := filepath.Ext(abs)
+		if ext != ".yml" && ext != ".yaml" {
+			dbg.Warning("Not a yaml file by extension: %s", abs)
+			continue
+		}
+		sources = append(sources, abs)
+	}
+	if len(sources) == 0 {
+		dbg.Warning("Resolving sources came up empty")
+	}
+	return Fileish{path, sources}
+}
+
+func (f Fileish)Read() []byte {
+	data := []byte{}
+	for _, source := range f.sources {
+		file, err := ioutil.ReadFile(source)
+		if err != nil {
+			dbg.Warning("Unable to read source: %s (%v)", source, err)
+			continue
+		}
+		data = append(data, file...)
+	}
+	return data
 }
--- sys/fs_test.go
+++ sys/fs_test.go
@@ -33,3 +33,56 @@ func Test_TempFilePathReturnsUniquePath(t *testing.T) {
 	os.Remove(temp1)
 	os.Remove(temp2)
 }
+
+func Test_NewFileishLoadsGlobFiles(t *testing.T) {
+	expected := 2
+	file := NewFileish("../examples/*.yml")
+	if len(file.sources) != expected {
+		t.Log(file.sources)
+		t.Fatalf("expected exactly %d example yamls, got %d", expected, len(file.sources))
+	}
+}
+
+func Test_NewFileishGlobRequiresYamlFiles(t *testing.T) {
+	expected := 0
+	file := NewFileish("../sys/*")
+	if len(file.sources) != expected {
+		t.Log(file.sources)
+		t.Fatalf("expected exactly %d sys yamls, got %d", expected, len(file.sources))
+	}
+}
+
+func Test_NewFileishLoadsSingleFile(t *testing.T) {
+	expected := 1
+	file := NewFileish("../examples/self.yml")
+	if len(file.sources) != expected {
+		t.Log(file.sources)
+		t.Fatalf("expected exactly %d yamls, got %d", expected, len(file.sources))
+	}
+}
+
+func Test_FileishReadsSingleYamlFile(t *testing.T) {
+	path := "../examples/self.yml"
+	stat1, _ := os.Stat(path)
+	file := NewFileish(path)
+	content := file.Read()
+	if int64(len(content)) != stat1.Size() {
+		t.Fatalf("expected %d bytes content, but got %d",
+			stat1.Size(), len(content))
+	}
+}
+
+func Test_FileishReadsGlobbedYamlFile(t *testing.T) {
+	paths := []string{"../examples/self.yml", "../examples/first.yml"}
+	var sumBytes int64
+	for _, path := range paths {
+		stat, _ := os.Stat(path)
+		sumBytes += stat.Size()
+	}
+	file := NewFileish("../examples/*.yml")
+	content := file.Read()
+	if int64(len(content)) != sumBytes {
+		t.Fatalf("expected %d bytes content, but got %d",
+			sumBytes, len(content))
+	}
+}
